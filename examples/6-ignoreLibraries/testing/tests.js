71f25cdf68c3eabcd977ecf138ef4df1f2db38f6
Raven <c.colijn@twologs.com>
2022-04-02 11:35:06 +0200

    Made test wording clearer on why we expect a failed test and what is to be seen from it

--- examples/6-ignoreLibraries/testing/tests.js
+++ examples/6-ignoreLibraries/testing/tests.js
@@ -5,11 +5,11 @@ let jazil = new Jazillionth(options)
 let mainPage = jazil.AddPageToTest('main', '../main.html', ['Summer'])
 
 
-jazil.AddTestSet(mainPage, 'Summer tests', {
-  'Summer should know 1 + 1': function(jazil) {
+jazil.AddTestSet(mainPage, 'Library files not in call stack', {
+  'Failing a test to force a call stack': function(jazil) {
     TestSummer(
-      1, 1, (summerResult, correctResult) => {
-        jazil.ShouldBe(summerResult, correctResult)
+      1, 2, (summerResult, correctResult) => {
+        jazil.ShouldBe(summerResult, correctResult, 'testLibrary.js should not show in the call stack')
       }
     )
   }
